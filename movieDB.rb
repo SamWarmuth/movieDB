afb883eaad0330319f20d2a734d4246c20dea66f
Sam Warmuth <samwarmuth@nawrml-16eng01.na.corp.storaenso.com>
2009-10-27 17:39:48 -0500

    allow movies to be renamed

--- movieDB.rb
+++ movieDB.rb
@@ -68,7 +68,7 @@ end
 post '/editMovie/:key' do
 	movie = Movie.get(params[:key])
 	movie.destroy! if !movie.nil?
-	movie = Movie.new(:release_year => params[:release_year].to_i, :length => params[:length].to_i, :mpaa_rating => params[:mpaa_rating], :plot => params[:plot].gsub("\r\n","<br />"))
+	movie = Movie.new(:title => params[:title], :release_year => params[:release_year].to_i, :length => params[:length].to_i, :mpaa_rating => params[:mpaa_rating], :plot => params[:plot].gsub("\r\n","<br />"))
 	params[:cast].split(/[ ]?,[ ]?/).each do |name|
 		actor = Actor.get(name)
 		actor = Actor.new( :name => name) if (actor.nil?)
